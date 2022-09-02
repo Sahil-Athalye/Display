4063608f447e508263bb9b1d02e1f555fd1bd1dd
Sahil-Athalye <89952559+Sahil-Athalye@users.noreply.github.com>
2022-09-02 12:25:13 -0500

    Delete File System/Code directory

--- File System/Code/SimpleFileFactory.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "AbstractFileFactory.h"
-// Studio 18 - simplefilefactory class declaration goes here
-class SimpleFileFactory : public AbstractFileFactory {
-public:
-	virtual AbstractFile* createFile(std::string) override;
-};
